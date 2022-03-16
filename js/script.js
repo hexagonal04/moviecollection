162b5f162ba822d0a252f0760270754350ebe7d8
Bayu Alamsah <49319053+hexagonal04@users.noreply.github.com>
2022-03-16 11:42:00 +0700

    add files via upload
    + index.html
    + folder js and script.js

---
+++ js/script.js
@@ -0,0 +1,96 @@
+function searchMovie(){
+    // empty the value of #movie-list before request data
+    $('#movie-list').html('');
+
+    // get json data using OMDb's API
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'dc881bfc',
+            's': $('#search-input').val()
+        },
+        success: function(result){
+            if (result.Response == "True"){
+                let movies = result.Search;
+                $.each(movies, function(i, data){
+                    $('#movie-list').append(`
+                    <div class="col-md-4">
+                        <div class="card mb-3">
+                            <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">`+data.Title+`</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">`+data.Year+`</h6>
+                                <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+data.imdbID+`">See Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+                });
+
+                // empty the value of search input after success
+                $('#search-input').val('');
+
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">`+ result.Error + `</h1>
+                    </div>
+                
+                `)
+            }
+        }
+    });
+}
+
+
+//add event
+$('#search-button').on('click', function(){
+   searchMovie();
+});
+
+$('#search-input').on('keyup', function(e){
+    if (e.keyCode === 13){
+        searchMovie();
+    }
+});
+
+//find element movie-list, on click = see-detail, run function
+$('#movie-list').on('click', '.see-detail', function(){
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'dc881bfc',
+            'i': $(this).data('id')
+        },
+        success: function (movie) {
+            if( movie.Response === "True") {
+
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="`+ movie.Poster +`" class="img-fluid">
+                            </div>
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                                    <li class="list-group-item">Released: `+ movie.Released +`</h3></li>
+                                    <li class="list-group-item">Genre: `+ movie.Genre +`</h3></li>
+                                    <li class="list-group-item">Director: `+ movie.Director +`</h3></li>
+                                    <li class="list-group-item">Actors: `+ movie.Actors +`</h3></li>
+                                    <li class="list-group-item">Plot: `+ movie.Plot +`</h3></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `)
+
+            }
+        }
+    });
+
+})
